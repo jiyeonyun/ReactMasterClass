798ac4d6ac00b7fd457b66584a4664f28fa50891
jiyeonyun <jiyeonyun43@gmail.com>
2022-05-26 09:36:39 +0900

    created styled Components

--- reactmasterclass/src/App.jsx
+++ reactmasterclass/src/App.jsx
@@ -1,7 +1,33 @@
-import styles from "./App.module.css";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.h1`
+  color: white;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Father>
+      <BoxOne />
+      <Text>Hello</Text>
+      <BoxTwo />
+    </Father>
+  );
 }
 
 export default App;
