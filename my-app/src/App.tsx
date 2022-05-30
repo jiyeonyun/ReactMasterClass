76446f1518e73d934b1359f7666788814000d675
jiyeonyun <jiyeonyun43@gmail.com>
2022-05-30 10:58:53 +0900

    theme

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,31 +2,17 @@ import React, { useState } from "react";
 import "./App.css";
 import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="username"
-          value={value}
-          onChange={onChange}
-        />
-        <button>log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
 
