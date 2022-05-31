804d2dde1ac257f306ca7036a961644880ab86c7
jiyeonyun <jiyeonyun43@gmail.com>
2022-05-31 13:55:37 +0900

    nestedRoutes

--- my-app/src/routes/Coins.tsx
+++ my-app/src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import { stringify } from "querystring";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -74,6 +75,7 @@ const Coins = () => {
       setLoading(false);
     })();
   }, []);
+
   return (
     <Container>
       <Header>
