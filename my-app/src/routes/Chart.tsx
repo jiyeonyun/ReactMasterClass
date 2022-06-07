0d7032cfd3b055b5f4c463db71dd052a4287a921
jiyeonyun <jiyeonyun43@gmail.com>
2022-06-07 16:13:23 +0900

    Price chart

--- my-app/src/routes/Chart.tsx
+++ my-app/src/routes/Chart.tsx
@@ -3,15 +3,75 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { fetchCoinHistory } from "./api";
 import { useOutletContext } from "react-router-dom";
+import ReactApexChart from "react-apexcharts";
 interface ChartProps {
   coinId: string;
 }
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 const Chart = () => {
   const { coinId } = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 3,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
